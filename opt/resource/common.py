c3aa715aa71a154b5278a25ddb31e5e9cb752262
mkr (Michael Kriegel) <m.kriegel@oblamatik.ch>
2018-09-27 10:02:25 +0200

    printf

--- opt/resource/common.py
+++ opt/resource/common.py
@@ -6,5 +6,4 @@ def get_newest(path):
         os.makedirs(path + str(version))
     while os.path.exists(path + str(version)):
         version = version + 1
-    print("newest version " + str(version))
     return version - 1
