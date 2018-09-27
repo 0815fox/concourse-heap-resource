7059d13241f9de5cb15fb19dfda540a7db93ec33
mkr (Michael Kriegel) <m.kriegel@oblamatik.ch>
2018-09-27 09:00:11 +0200

    create version 0 if not exists (empty)

--- opt/resource/common.py
+++ opt/resource/common.py
@@ -2,6 +2,8 @@ import os
 
 def get_newest(path):
     version = 0
+    if not os.path.exists(path + str(version)):
+        os.makedirs(path + str(version))
     while os.path.exists(path + str(version)):
         version = version + 1
     return version - 1
